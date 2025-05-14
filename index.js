2145f1af4c3d0df1f313bd1dc1957833c00b9fc6
Your Name <you@example.com>
2025-05-14 16:24:51 +0530

    adding react app

---
+++ index.js
@@ -0,0 +1,39 @@
+console.log("the proof of the ")
+// class animal{
+//     constructor(name){
+//         this.name = name
+//     }
+// }
+// class birds extends animal{
+//     constructor(name){
+//         super(name)
+
+
+//     }
+x => console.log(x)
+// }
+// const obj = new birds("shashank")
+// console.log(obj.name)
+// function lol(){
+//     let name ="the shashank vash"
+//     console.log("hello "+name)
+
+// }
+// lol()
+// function tester(para1,para2){
+//     return para1 + " " + para2
+
+// }
+// console.log(tester("shashank","vaish"))
+// function kol(){
+//     let op = prompt("enter number 1 ")
+//     let ou = prompt("enter 2nd number ")
+//     return parseInt(op)  + parseInt(ou)
+// }
+// kol()
+
+// x(90)
+
+let spread = ["the",'shahsank','kol']
+let y = ['yash ', ...spread]
+console.log(y)
